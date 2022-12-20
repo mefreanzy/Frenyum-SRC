e41e03416db42f6da510c4d7b3944297e6759db2
Yiğit Berke Taburlu <92319842+mefreanzy@users.noreply.github.com>
2022-12-20 22:44:51 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
 fn main() {
-    println!("Ready!");
+    println!("Type 'help' to list command.");
+
+    let chain: Arc<Mutex<Vec<Block>>> = Arc::new(Mutex::new(Vec::new()));
+    let mut peers: Vec<String> = Vec::new();
 }
