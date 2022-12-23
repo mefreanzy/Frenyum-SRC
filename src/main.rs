40676059a5f99fd664aa3da74a41dd7fe3f118ca
Yiğit Berke Taburlu <92319842+mefreanzy@users.noreply.github.com>
2022-12-24 00:27:54 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
+use std::sync::Mutex;
+
+use Frenyum::skeleton::block::Block;
+use Frenyum::RPC::peer::{Node, Message};
+
 fn main() {
     println!("Type 'help' to list command.");
 
-    let chain: Arc<Mutex<Vec<Block>>> = Arc::new(Mutex::new(Vec::new()));
+    let memory: Vec<Block> = Vec::new();
+    let memory_lock: Mutex<Vec<Block>>  = Mutex::new(Vec::new());
+
     let mut peers: Vec<String> = Vec::new();
 }
