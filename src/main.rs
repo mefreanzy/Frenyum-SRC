d9c1b0aef11f43a3d173f933bd3f5439bc9f356d
Yiğit Berke Taburlu <92319842+mefreanzy@users.noreply.github.com>
2022-12-28 06:32:18 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
 use std::sync::Mutex;
+use std::thread::spawn;
+use std::net::TcpListener;
+use std::io::{Read, Write, Error};
+use std::io;
 
 use Frenyum::skeleton::block::Block;
 use Frenyum::RPC::peer::{Node, Message};
@@ -6,8 +10,47 @@ use Frenyum::RPC::peer::{Node, Message};
 fn main() {
     println!("Type 'help' to list command.");
 
-    let memory: Vec<Block> = Vec::new();
-    let memory_lock: Mutex<Vec<Block>>  = Mutex::new(Vec::new());
+    // Create new node structure
+    let adrr: String = "0.0.0.0".to_string();
+    let nport: u16 = 1000;
+    let mut node = Node::new(adrr, nport);
 
-    let mut peers: Vec<String> = Vec::new();
+    spawn(move || {
+        let listener = TcpListener::bind(&format!("{}:{}", node.adress, node.port)).unwrap();
+
+        for income in listener.incoming()
+        {
+            let mut stream = income.unwrap();
+            println!("New connection accepted: {}", stream.peer_addr().unwrap());
+
+            let message = Node::receive_message(&mut stream).unwrap();
+            println!("Message received");
+
+            match message
+            {
+                Message::Block(block) => {
+                    let lock = node.memory_lock.lock().unwrap();
+
+                    node.memory.push(block);
+                }
+            }
+        }
+
+    });
+
+    loop
+    {
+        let mut input = String::new();
+        println!("Enter RPC command: ");
+        io::stdin().read_line(&mut input).unwrap();
+        let input: Vec<&str> = input.trim().split(" ").collect();
+        let rpc = input[0];
+
+        if rpc == "help"
+        {
+            println!("RPC commands:
+                     help: List available RPC commands
+                     add_tx: Add new transaction data");
+        }
+    }
 }
