a45bea6d7bd6ee4f6a4ffdbd2e69806d6768b4de
Yiğit Berke Taburlu <92319842+mefreanzy@users.noreply.github.com>
2022-12-30 19:19:07 +0300

    Update blockchain.rs

--- src/skeleton/blockchain.rs
+++ src/skeleton/blockchain.rs
@@ -1,1 +1,20 @@
 use crate::skeleton::block::Block;
+use rocksdb::{DB, Options};
+
+struct Blockchain
+{
+    db: DB,
+}
+
+impl Blockchain
+{
+    fn new(path: &str) -> Blockchain
+    {
+        let mut opts = Options::default();
+        opts.create_if_missing(true);
+
+        let db = DB::open(&opts, path).unwrap();
+
+        Blockchain { db }
+    }
+}
