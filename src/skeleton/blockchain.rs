bf6ae82adc7fb0a5fc685c88d6c658feacbbf8e2
Yiğit Berke Taburlu <92319842+mefreanzy@users.noreply.github.com>
2023-01-28 14:01:44 +0300

    Update blockchain.rs

--- src/skeleton/blockchain.rs
+++ src/skeleton/blockchain.rs
@@ -1,20 +1,69 @@
 use crate::skeleton::block::Block;
-use rocksdb::{DB, Options};
+use crate::skeleton::transaction::TransactionData;
+
+use mongodb::{Client, Collection, Database};
+use mongodb::bson::doc;
+use mongodb::bson;
 
 struct Blockchain
 {
-    db: DB,
+    client: Client,
+    db: Database,
+    blocks: Collection,
+    txs: Collection,
 }
 
 impl Blockchain
 {
-    fn new(path: &str) -> Blockchain
+    async fn new(uri: &str, db_name: &str, blockdb_name: &str, txdb_name: &str) -> Result<Blockchain, mongodb::error::Error>
     {
-        let mut opts = Options::default();
-        opts.create_if_missing(true);
+        let client = match Client::with_uri_str(uri).await {
+            Ok(client) => client,
+            Err(e) => return Err(e),
+        };
 
-        let db = DB::open(&opts, path).unwrap();
+        let db = client.database(db_name);
+        let blocks = db.collection(blockdb_name);
+        let txs = db.collection(txdb_name);
 
-        Blockchain { db }
+        Ok(Blockchain {
+            client,
+            db,
+            blocks,
+            txs,
+        })
+    }
+
+    async fn add_db_tx(&self, transaction: &TransactionData)
+    {
+        let bson_transaction = bson::to_document(transaction);
+
+        let bson_transaction = doc! {
+            "from": transaction.from,
+            "to": transaction.to,
+            "amount": transaction.amount,
+            "timestamp": transaction.timestamp,
+        };
+        self.txs
+            .insert_one(bson_transaction, None)
+            .await.expect("Failed to insert transaction.");
+    }
+
+    async fn add_db_block(&self, block: &Block)
+    {
+        let bson_block = bson::to_document(&block);
+        let tx_ids = block.vtx.iter().map(|tx| bson::to_bson(tx).unwrap()).collect::<Vec<_>>();
+
+        let bson_block = doc! {
+            "index": block.index,
+            "hash": block.get_hash(),
+            "previous_hash": block.previous_hash,
+            "time": block.time,
+            "nonce": block.nonce,
+            "tx_ids": tx_ids,
+        };
+        self.blocks
+            .insert_one(bson_block, None)
+            .await.expect("Failed to insert block.");
     }
 }
